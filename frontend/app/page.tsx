a6f12ed9d64ee05d22db4e09c4d628ec97f2b199
Lernobug <gunjanaggarwal12345.com>
2024-06-26 12:39:24 +0530

    create board

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
+"use client"
+import { useDraw } from "@/hooks/useDraw";
 import Image from "next/image";
 
 export default function Home() {
+  const {canvasRef}=useDraw()
+
   return (
-    <div >
-     Hello
+    <div  className="w-screen h-screen bg-white flex justify-center items-center">
+      <canvas ref={canvasRef} width={750} height={550} className="border border-black rounded-md"></canvas>  
     </div>
   );
 }
