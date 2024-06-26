94ce90077a316a1deede335e7c5488dc3a9cf97d
Sumit <sumitgarg380@gmail.com>
2024-06-26 16:22:55 +0530

    handle Mouse down

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,40 @@
-"use client"
+"use client";
 import { useDraw } from "@/hooks/useDraw";
 import Image from "next/image";
 
 export default function Home() {
-  const {canvasRef}=useDraw()
+  const { canvasRef, handleMouseDown } = useDraw(drawLine);
+
+  const drawLine = ({ ctx, currentPoint, prevPoint }: Draw) => {
+    const { x: currX, y: currY } = currentPoint;
+    const color = "black";
+    const width = 2;
+
+    let startPoint = prevPoint || currentPoint;
+    if (!ctx) return;
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+  };
 
   return (
-    <div  className="w-screen h-screen bg-white flex justify-center items-center">
-      <canvas ref={canvasRef} width={750} height={550} className="border border-black rounded-md"></canvas>  
+    <div className="w-screen h-screen bg-white flex justify-center items-center">
+      <canvas
+        onMouseDown={handleMouseDown}
+        ref={canvasRef}
+        width={750}
+        height={550}
+        className="border border-black rounded-md"
+      ></canvas>
     </div>
   );
 }
--- frontend/hooks/useDraw.ts
+++ frontend/hooks/useDraw.ts
@@ -1,31 +1,39 @@
-"use client"
-import { use, useEffect, useRef } from "react"
+"use client";
+import { use, useEffect, useRef, useState } from "react";
 
-export const useDraw  = () => {
+export const useDraw = (
+  onDraw: ({ ctx, currentPoint, prevPoint }: Draw) => void
+) => {
+  const [clicked, setClicked] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const prevPoint = useRef<Point | null>(null);
+    const handleMouseDown = () => {
+    setClicked(true);
+    };
 
-    const canvasRef=useRef<HTMLCanvasElement>(null)
-   useEffect(()=>{
-       const handler=(e:MouseEvent)=>{
-        const currentPoint=computePointInCanvas(e);
-        const ctx=canvasRef.current?.getContext('2d')
-        if(!ctx || !currentPoint) return
-       }
 
-       const computePointInCanvas = (e:MouseEvent)=>{
-        const canvas=canvasRef.current;
-        if(!canvas) return 
-        const rect=canvas.getBoundingClientRect();
-        const x=e.clientX-rect.left
-        const y=e.clientY-rect.right
-        console.log( {x:x,y:y})
-        console.log( {rectleft:rect.left,rectright:rect.right})
-        return {x,y};
-       }
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      const currentPoint = computePointInCanvas(e);
+      const ctx = canvasRef.current?.getContext("2d");
+      if (!ctx || !currentPoint) return;
+    };
 
-       canvasRef.current?.addEventListener('mousemove',handler)
+    const computePointInCanvas = (e: MouseEvent) => {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+      const rect = canvas.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      console.log({ x: x, y: y });
+      console.log({ rectleft: rect.left, rectright: rect.right });
+      return { x, y };
+    };
 
-       return ()=> canvasRef?.current?.removeEventListener('mousemove',handler)
-   },[])
+    canvasRef.current?.addEventListener("mousemove", handler);
 
-   return {canvasRef}
-}
+    return () => canvasRef?.current?.removeEventListener("mousemove", handler);
+  }, []);
+
+  return { canvasRef, handleMouseDown };
+};
